26de608d1581b5c34fdfddb58543e38820bdfb58
pidrmasiin <petteri.paakkonen@helsinki.fi>
2016-11-09 13:13:11 +0200

    checkstyle ja pit lisätty

--- OmahaHelp/src/main/java/omahahelp/Main.java
+++ OmahaHelp/src/main/java/omahahelp/Main.java
@@ -23,24 +23,23 @@ public class Main {
         // TODO code application logic here
         Cards cards = new Cards();
         cards.addCards();
-        
+
         Draw draw = new Draw(cards);
-       
+
         HandsValues values = new HandsValues();
         values.drawCardsToHand();
         System.out.println(values.getHand());
         values.setCardsToHand(cards.getCard(13), cards.getCard(22), cards.getCard(25), cards.getCard(24), cards.getCard(23));
         System.out.println(values.getHand());
-        if(values.checkFlush()){
+        if (values.checkFlush()) {
             System.out.println("väri");
         }
-        
-        
-        if (values.checkStraight()){
+
+        if (values.checkStraight()) {
             System.out.println("suora");
         }
         System.out.println(values.getHand());
-        if (values.checkRoyalFlush()){
+        if (values.checkRoyalFlush()) {
             System.out.println("värisuora");
         }
     }
